da59dab2e9aee9aa8b8cac899983d9436a4db0ed
I Putu Weda Wiguna <138590629+wedaiputu@users.noreply.github.com>
2024-08-07 19:09:07 +0530

    Initial Commit

---
+++ testGIthub/testGIthubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGIthubApp.swift
+//  testGIthub
+//
+//  Created by wedawiguna on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testGIthubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
